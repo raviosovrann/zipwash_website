4a48d9e00bbe3ea88eecc63c6d7c7cda189c2def
Anvar Nosirov <anvar-nosirov@ubuntu.mynetworksettings.com>
2025-03-13 00:54:54 -0400

    Got rid of My Accounts dropdown, replaced with Sign In button, then separated Log In and Sign Up Forms

--- main/views.py
+++ main/views.py
@@ -9,3 +9,9 @@ def pricing(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
